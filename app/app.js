9ccd82e4403e6d4b9b483fcc9efcc06f2444929d
Arash Abbasi <arashabbasi@Arashs-MacBook-Pro.local>
2019-07-08 18:01:34 -0700

    Slideshow added

--- app/app.js
+++ app/app.js
@@ -6,6 +6,23 @@
 
 */
 
+//Slideshow feature
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
 //localStorage functions
 var createItem = function(key, value) {
   return window.localStorage.setItem(key, value);
